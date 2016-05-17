767652ad715d8d61314e8be126a7fc1b7b85801c
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2016-05-17 18:05:49 +0700

    Made the redux store init reactively set on db data
    - redux store is now initialised with data based on data in the DB
    - runs reactively, so that data will be reactively set once collection subscription is ready

--- imports/startup/client/redux-store-init.js
+++ imports/startup/client/redux-store-init.js
@@ -1,12 +1,21 @@
 import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import { Documents } from '/imports/api/documents/documents';
 import { Store } from '/imports/ui/redux/store';
 import { DOCUMENT_COUNT_SET } from '/imports/ui/redux/actions';
 
 Meteor.startup(() => {
-  const docCount = 0;
+  Tracker.autorun(() => {
+    const subscription = Meteor.subscribe('documents');
 
-  Store.dispatch({
-    type: DOCUMENT_COUNT_SET,
-    docCount,
+    if (subscription.ready()) {
+      const documents = Documents.find();
+      const docCount = documents.count();
+
+      Store.dispatch({
+        type: DOCUMENT_COUNT_SET,
+        docCount,
+      });
+    }
   });
 });
