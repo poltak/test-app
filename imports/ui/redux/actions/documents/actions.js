3839fa35c96d32905bf01732b6e1c7563e9b4c71
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2016-05-17 16:56:14 +0700

    Added actions for each document method
    - actions are bound to store dispatches

---
+++ imports/ui/redux/actions/documents/actions.js
@@ -0,0 +1,48 @@
+import {
+  insertDocument,
+  updateDocument,
+  removeDocument,
+} from '/imports/api/documents/methods';
+import * as actionTypes from './actionTypes';
+import { Store } from '/imports/ui/redux/store';
+
+const buildAction = (type) => ({ type });
+
+export function insertDocumentAction({ title, successCb, errorCb }) {
+  return () => {
+    insertDocument.call({ title }, (error) => {
+      if (error) {
+        errorCb(error);
+      } else {
+        Store.dispatch(buildAction(actionTypes.DOCUMENT_INSERT));
+        successCb();
+      }
+    });
+  };
+}
+
+export function updateDocumentAction({ _id, update, successCb, errorCb }) {
+  return () => {
+    updateDocument.call({ _id, update }, (error) => {
+      if (error) {
+        errorCb(error);
+      } else {
+        Store.dispatch(buildAction(actionTypes.DOCUMENT_UPDATE));
+        successCb();
+      }
+    });
+  };
+}
+
+export function removeDocumentAction({ _id, successCb, errorCb }) {
+  return () => {
+    removeDocument.call({ _id }, (error) => {
+      if (error) {
+        errorCb(error);
+      } else {
+        Store.dispatch(buildAction(actionTypes.DOCUMENT_REMOVE));
+        successCb();
+      }
+    });
+  };
+}
